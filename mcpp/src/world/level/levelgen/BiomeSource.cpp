9f784a1e56c7d0d6b0dd38f5e248fc78bb810c9d
mtrdrgz <138630791+mtrdrgz@users.noreply.github.com>
2026-06-07 00:25:54 +0200

    Update BiomeSource biome collection

--- mcpp/src/world/level/levelgen/BiomeSource.cpp
+++ mcpp/src/world/level/levelgen/BiomeSource.cpp
@@ -1,5 +1,4 @@
 #include "BiomeSource.h"
-#include "NoiseBasedChunkGenerator.h"
 #include "OverworldBiomeBuilder.h"
 
 #include <algorithm>
@@ -52,29 +51,6 @@ std::string BiomeSource::getNoiseBiome(int quartX, int quartY, int quartZ) const
         static_cast<float>(m_router.ridges->compute(context)));
 
     return m_parameters.findValue(target);
-}
-
-std::string NoiseBasedChunkGenerator::getNoiseBiome(int quartX, int quartY, int quartZ) const {
-    return m_biomeSource ? m_biomeSource->getNoiseBiome(quartX, quartY, quartZ) : std::string{};
-}
-
-void NoiseBasedChunkGenerator::initializeDecorationFeatures(const feature::BiomeFeatures& biomeFeatures) {
-    m_decorationSourceBiomes.clear();
-    m_decorationFeaturesPerStep.clear();
-    m_decorationFeaturesReady = false;
-
-    if (!m_biomeSource) {
-        return;
-    }
-
-    // Java ChunkGenerator memoizes:
-    // FeatureSorter.buildFeaturesPerStep(List.copyOf(biomeSource.possibleBiomes()), ...)
-    m_decorationSourceBiomes = m_biomeSource->possibleBiomes();
-    m_decorationFeaturesPerStep = feature::FeatureSorter::buildFeaturesPerStep(
-        m_decorationSourceBiomes,
-        biomeFeatures,
-        true);
-    m_decorationFeaturesReady = true;
 }
 
 } // namespace mc::levelgen
