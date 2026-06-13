db53759bd7832dfbbb733e03d1c3428ef3bb5ac3
mtrdrgz <138630791+mtrdrgz@users.noreply.github.com>
2026-06-13 14:23:12 +0200

    Reject empty runtime asset packs

--- mcpp/cmake/VerifyAssetPack.cmake
+++ mcpp/cmake/VerifyAssetPack.cmake
@@ -8,3 +8,7 @@ endif()
 
 file(SIZE "${ASSET_BIN}" ASSET_BIN_SIZE)
 message(STATUS "Asset pack: ${ASSET_BIN} (${ASSET_BIN_SIZE} bytes)")
+
+if(ASSET_BIN_SIZE LESS 1048576)
+    message(FATAL_ERROR "Asset pack is too small; refusing to publish an asset-less executable")
+endif()
